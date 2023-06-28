721aae6ed7364bd2dee83b2f21557eb8d6102612
Anakin Nanthavongsa <oven4151@gmail.com>
2023-06-28 10:12:11 +0700

    test-action

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const fs = require("fs");
 const { generateKeyPairSync } = require("crypto");
 const { dateTimeFormatter } = require("./utils");
 const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 readline.question(`Input Length of keys : `, (len) => {
-    console.log(`Keys length: ${len}\n`);
-    readline.close();
+  console.log(`Keys length: ${len}\n`);
+  readline.close();
 
-    const { privateKey, publicKey } = generateKeyPairSync("rsa", {
-        modulusLength: parseInt(`${len}`),
-        privateKeyEncoding: {
-            type: "pkcs1",
-            format: "pem",
-        },
-        publicKeyEncoding: {
-            type: "pkcs1",
-            format: "pem",
-        },
-    });
+  const { privateKey, publicKey } = generateKeyPairSync("rsa", {
+    modulusLength: parseInt(`${len}`),
+    privateKeyEncoding: {
+      type: "pkcs1",
+      format: "pem",
+    },
+    publicKeyEncoding: {
+      type: "pkcs1",
+      format: "pem",
+    },
+  });
 
-    console.log(privateKey);
-    console.log(publicKey);
+  console.log(privateKey);
+  console.log(publicKey);
 });
